1a4447dcf8459330e651cb57e71a6e14ff082e35
Thaís Lescano <thaislescano@hotmail.com>
2019-02-05 16:08:12 -0200

    vertical axis, components

---
+++ Assets/SpaceshipMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+    float velMax = 5f;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        // um/frame :}
+        //Input.GetAxis("Vertical"); //float de -1 até 1
+        Vector3 pos = transform.position;
+
+        pos.y += Input.GetAxis("Vertical") * velMax * Time.deltaTime;
+        transform.position = pos;
+		
+	}
+    //fixedUpdate - quinze/frame - physics
+}
